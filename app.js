d53172ce126a99cad6b85172bd4a6fa3b266e0af
atlant04 <maxatlant17@gmail.com>
2019-11-20 03:12:41 -0500

    added client side

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const GO = require('./GO.js')
+const express = require('express')
+const app = express()
+app.use(express.static("public"))
 
+app.post('/api', (req, res) => {
+    GO.getStops().then(data => {
+        res.json({
+            status: 'success',
+            data: data 
+        })
+    })
+})
 
-GO.getBuses().then(data => console.log(data))
+app.listen(3000, () => console.log("Listening on port 3000"))
---
+++ public/client.js
@@ -0,0 +1,35 @@
+var mymap = L.map('mapid').setView([32.77, -84.39], 15);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYXRsYW50MDQiLCJhIjoiY2swc2c2NXNkMDJjNzNicGR6bjFnZHBqZiJ9.OBsA8h0eendA7wWQ6YyhoA'
+}).addTo(mymap);
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json'
+    }
+}
+
+fetch('/api', options)
+.then(response => response.json())
+.then(data => {
+    Object.values(data.data.stops).forEach(stop => {
+        const lat = stop.latitude
+        const lon = stop.longitude
+        var marker = L.marker([lat, lon]).addTo(mymap)
+        marker.bindPopup(`<b>${stop.name}</b>`).openPopup();
+    })
+})
+
+var lat = 33.77406
+var lon = -84.39192
+
+var marker = L.marker([lat, lon]).addTo(mymap)
+
+setInterval(() => {
+    marker.setLatLng([lat += 0.00001, lon += 0.00001])
+}, 100)
